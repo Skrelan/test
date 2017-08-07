d780f3eeac5dbd59e23889a09a8016895b4acf1f
skrelan <skrelan@gmail.com>
2017-08-07 15:50:43 -0700

    nkdgkg# Please enter the commit message for your changes. Lines starting

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ func visit(numbers []int, callback func(int)) {
 	}
 }
 func main() {
+	i := 1
+	fmt.Println("This is awesome")
+	fmt.Println("This a tutorial on clousers2!")
 	fmt.Println("This a tutorial on clousers!")
 	visit([]int{1, 2, 3, 4, 5, 6, 7, 8}, func(n int) {
 		fmt.Println(n)
